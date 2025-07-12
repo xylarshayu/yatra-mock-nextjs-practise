3f971da20e0af066d1396f326cd42301ee74f77f
xylarshayu <xylar.w@gmail.com>
2025-07-12 23:39:03 +0530

    feat: setting up project files

---
+++ yatra/src/pages/hotels/[id].tsx
@@ -0,0 +1,7 @@
+export default function Hotel() {
+  return (
+    <main>
+      This hotel
+    </main>
+  )
+}
---
+++ yatra/src/pages/hotels/index.tsx
@@ -0,0 +1,7 @@
+export default function HotelList() {
+  return (
+    <main>
+      Hotel list
+    </main>
+  )
+}
